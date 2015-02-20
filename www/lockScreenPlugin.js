07f6bbd250408b37876d70331f017813b08f2795
BorkoPhil <boris.filipovic@izstop.si>
2015-02-20 16:06:08 +0100

    Update lockScreenPlugin.js

--- www/lockScreenPlugin.js
+++ www/lockScreenPlugin.js
@@ -1,1 +1,16 @@
-// Java script code here.
+// Custom Lock Screen Plugin.
+// Created by Boris Filipović on 20/2/2015.
+// MIT Licensed.
+
+var lockScreen = {
+	var str = "borisTest";
+	setBackground: function(){
+		window.echo = function(str, callback) {
+				cordova.exec(callback, function(err) {
+					callback('Nothing to echo.');
+				}, "LockScreen", "lockscreen", []);
+		};
+	}
+}
+
+module.exports = lockScreen;
