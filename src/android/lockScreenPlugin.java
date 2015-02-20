6411b0d6f4efc5a0392ac9a7d721e84d9c7403df
BorkoPhil <boris.filipovic@izstop.si>
2015-02-20 16:04:39 +0100

    Update lockScreenPlugin.java

--- src/android/lockScreenPlugin.java
+++ src/android/lockScreenPlugin.java
@@ -1,1 +1,39 @@
-// Java file here.
+package com.boris.plugintestproject;
+
+import android.content.BroadcastReceiver;
+import android.app.KeyguardManager;
+import android.app.Service;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.os.IBinder;
+import android.app.Activity;
+import android.os.Bundle;
+import android.telephony.PhoneStateListener;
+import android.telephony.TelephonyManager;
+import android.view.KeyEvent;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.RelativeLayout.LayoutParams;
+import android.content.Context;
+import org.apache.cordova.CallbackContext;
+import org.apache.cordova.CordovaPlugin;
+import org.apache.cordova.PluginResult;
+import org.json.JSONArray;
+import org.json.JSONException;
+//import receiver.lockScreenReeiver;
+
+public class LockScreen extends CordovaPlugin {
+	
+	public static final String ACTION = "lockscreen";
+
+	@Override
+	public boolean execute(String action, JSONArray args, final CallbackContext callbackContext) throws JSONException {
+		if(action.equals(ACTION)){
+			return "skruju";
+		} else {
+			return false;
+		}
+	}
+}
